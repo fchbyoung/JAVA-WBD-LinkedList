ab31ea81a24122ebda5dbda9a36abe36eb983ba2
long <fchbyoung1010@gmail.com>
2019-07-15 23:27:52 +0700

    LinkedList

---
+++ src/main/java/com/btcodegym/LinkedListTest.java
@@ -0,0 +1,11 @@
+package com.btcodegym;
+
+public class LinkedListTest {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList(10);
+        linkedList.addFirst(5);
+        linkedList.addFirst(4);
+        linkedList.add(1,6);
+        linkedList.printList();
+    }
+}
